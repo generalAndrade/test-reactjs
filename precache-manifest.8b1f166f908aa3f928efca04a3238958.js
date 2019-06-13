9518ad2830814dbcff760793876a23882004e1d7
Leonardo Gerheim de Andrade <general.andrade@gmail.com>
2019-06-13 17:41:29 -0300

    Updates

---
+++ precache-manifest.8b1f166f908aa3f928efca04a3238958.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c23d9de880fc253a10dd6e14563980e2",
+    "url": "/reactjs-photo/index.html"
+  },
+  {
+    "revision": "6f8e038ff3654355ea43",
+    "url": "/reactjs-photo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/reactjs-photo/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "6f8e038ff3654355ea43",
+    "url": "/reactjs-photo/static/js/main.5b5d5e5e.chunk.js"
+  },
+  {
+    "revision": "a1538d7b619cce961fba",
+    "url": "/reactjs-photo/static/js/runtime~main.711a86cb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjs-photo/static/media/logo.5d5d9eef.svg"
+  }
+]);
